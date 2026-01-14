c74b9c2136d3db4e18967a066291f13989604d5f
woody <johannes8873@gmail.com>
2026-01-14 19:38:17 +0800

    feat:UI fixed

--- app/app.vue
+++ app/app.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
-    <NuxtLayout>
-      <NuxtPage />
-    </NuxtLayout>
+    <UApp>
+      <NuxtLayout>
+        <NuxtPage />
+      </NuxtLayout>
+    </UApp>
   </div>
 </template>
