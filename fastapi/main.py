6522610c6910f3739e35363c7bd316fe4f9bf9d8
woody <johannes8873@gmail.com>
2026-01-14 19:59:42 +0800

    fix:update the CORS

--- fastapi/main.py
+++ fastapi/main.py
@@ -15,7 +15,7 @@ app = FastAPI(
 origins = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "https://cmbfrontend.vercel.app/",
+    "https://cmbfrontend.vercel.app",
 ]
 
 app.add_middleware(
